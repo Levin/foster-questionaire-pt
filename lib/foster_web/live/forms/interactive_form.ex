3a8c3d8b9c5824afdb54d07decdd0051b9666f20
levin <mail.an.levin@gmail.com>
2024-09-28 09:37:01 +0000

    foster format

--- lib/foster_web/live/forms/interactive_form.ex
+++ lib/foster_web/live/forms/interactive_form.ex
@@ -9,9 +9,23 @@ defmodule FosterWeb.InteractiveForm do
 
   def render(assigns) do
     ~H"""
+    <div>
       <div>
         Welcome to the Hackathon on Foster Care
       </div>
+      <.simple_form 
+        for={}
+        >
+      <.label>Your Name</.label>
+      <.input name="name" value="" placeholder="Name"/>
+      <.label>Your Age</.label>
+      <.input name="age" value="" placeholder="Name"/>
+      <.label>Your Family Members</.label>
+      <.input name="members" value="" placeholder="Name"/>
+      <.label>Amount of spare Rooms</.label>
+      <.input name="" value="" placeholder="Name"/>
+      </.simple_form>
+    </div>
     """
   end
 end
