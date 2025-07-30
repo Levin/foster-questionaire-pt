e1c6cf3ad7c20d5c561f6c04e1b07d8c797b73ee
palec87 <david@stanka.de>
2025-07-30 16:54:14 +0100

    remove old commented code

--- lib/foster_web/live/components/info_2_from_base.ex
+++ lib/foster_web/live/components/info_2_from_base.ex
@@ -37,36 +37,3 @@ defmodule FosterWeb.Components.Info2 do
     """
   end
 end
-
-
-###### Replicate some plot here #######
-# defmodule FosterWeb.Components.Dashboard.AgeSpans do
-#   alias Foster.Answers.Answer
-#   use FosterWeb, :live_component
-
-#   def mount(socket) do
-#     spans =
-#       Foster.Answers.all_answers()
-#       |> Enum.group_by(fn answer -> answer.body["agespan"] end)
-#       |> Enum.map(fn {groupname, answers} -> %{contagem: length(answers), opções: groupname} end)
-
-
-#     IO.inspect(spans, label: "spans")
-
-#     plot = Tucan.bar(spans, "opções", "contagem",
-#       tooltip: true,
-#       orient: :horizontal,
-#       corner_radius: 3)
-#       |> Tucan.set_title("Faixa etária de todos os participantes", anchor: :middle, offset: 15)
-#       |> VegaLite.to_spec()
-
-
-#     {:ok, push_event(socket, "draw_ages", %{"spec" => plot})}
-#   end
-
-#   def render(assigns) do
-#     ~H"""
-#     <div id="ages-chart" phx-hook="Dashboard"/>
-#     """
-#   end
-# end
